c429aa1cf65083ec6c3be88781220d4713bb504e
Shai07 <94008671+Shai07@users.noreply.github.com>
2026-03-16 18:54:57 -0400

    implemented fetch_chain and get_atm_iv

--- vrp/iv.py
+++ vrp/iv.py
@@ -1,24 +1,52 @@
+# iv.py
 import numpy as np
 import pandas as pd
-from soupsieve import closest
 from scipy.stats import norm
 import yfinance as yf
+from datetime import datetime, timedelta
 
 
-def get_atm_iv(chain, spot: float, target_dte, method="nearest_strike") -> float:
-    pass
+def get_atm_iv(ticker: str, target_dte, method="nearest_strike", r: float = 0.05) -> tuple[float, float, float]:
+    calls, puts, S, closest_exp = fetch_chain(ticker, target_dte)
+    actual_dte = (datetime.strptime(closest_exp, "%Y-%m-%d") - datetime.today()).days
+    T = actual_dte / 365
+    
+    calls_filtered = calls[(calls['bid'] > 0) & (calls['ask'] > 0) & (calls['openInterest'] > 0)]
+    atm_call = calls_filtered.iloc[(calls_filtered['strike'] - S).abs().argsort().iloc[0]]
 
-def fetch_chain(ticker: str, target_dte: int) -> tuple[pd.DataFrame, pd.DataFrame, float]:
+    puts_filtered = puts[(puts['bid'] > 0) & (puts['ask'] > 0) & (puts['openInterest'] > 0)]
+    atm_put = puts_filtered.iloc[(puts_filtered['strike'] - S).abs().argsort().iloc[0]]
+
+    call_market_price = (atm_call['bid'] + atm_call['ask']) / 2
+    put_market_price = (atm_put['bid'] + atm_put['ask']) / 2
+    
+    call_sigma = implied_volatility(call_market_price, S, atm_call['strike'], T, r, "call")
+    put_sigma = implied_volatility(put_market_price, S, atm_put['strike'], T, r, "put")
+
+    avg_iv = (call_sigma + put_sigma) / 2
+    
+    return avg_iv, call_sigma, put_sigma
+    
+
+def fetch_chain(ticker: str, target_dte: int) -> tuple[pd.DataFrame, pd.DataFrame, float, str]:
     # returns calls, puts, spot
+    target_date = datetime.today() + timedelta(days=target_dte)
     tk = yf.Ticker(ticker)
     expirations = tk.options          # tuple of date strings
+    
+    closest_exp = min(expirations, key=lambda exp: abs(datetime.strptime(exp, "%Y-%m-%d") - target_date))
+
+    
     spot = tk.fast_info['last_price']
-    chain = tk.option_chain(expirations[0])  # curr 
+    chain = tk.option_chain(closest_exp)  # curr 
     calls = chain.calls
     puts = chain.puts
 
+    return calls, puts, spot, closest_exp
+    
 
-def implied_volatility(market_price: float, S: float, K: float, T: float, r: float) -> float:
+
+def implied_volatility(market_price: float, S: float, K: float, T: float, r: float, option_type="call") -> float:
     """
     f(sigma) = black_scholes_call(S, K, T, r, sigma) - market_price
     Vega=S⋅T⋅N'(d1)
@@ -29,14 +57,14 @@ def implied_volatility(market_price: float, S: float, K: float, T: float, r: flo
     continue calculating until |f(sigma)| <= threshold error
     """
     sigma = 0.2
-    error = black_scholes_call(S, K, T, r, sigma) - market_price
+    error = black_scholes(S, K, T, r, sigma, option_type) - market_price
     iterations = 0
     while abs(error) > 0.005:
         d1 = (np.log(S/K) + (r + np.square(sigma) / 2) * T) / (sigma * np.sqrt(T))
         vega = S * np.sqrt(T) * norm.pdf(d1)
         sigma = sigma - error / vega
 
-        error = black_scholes_call(S, K, T, r, sigma) - market_price
+        error = black_scholes(S, K, T, r, sigma, option_type) - market_price
         iterations += 1
         if iterations > 100:
             raise ValueError(f"IV solver did not converge after 100 iterations. Last sigma: {sigma:.4f}")
@@ -44,20 +72,22 @@ def implied_volatility(market_price: float, S: float, K: float, T: float, r: flo
     return sigma
 
 
-def black_scholes_call(S: float, K: float, T: float, r: float, sigma: float) -> float:
+def black_scholes(S: float, K: float, T: float, r: float, sigma: float, option_type="call") -> float:
     """
     C=S⋅N(d1)-K⋅e-rT⋅N(d2)
+    P = K⋅e-rT⋅N(-d2) - S⋅N(-d1)
 
     """
+    multiplier = 1 if option_type == "call" else -1
     d1 = (np.log(S/K) + (r + np.square(sigma) / 2) * T) / (sigma * np.sqrt(T))
     d2 = d1 - (sigma * np.sqrt(T))
     
 
-    return S * norm.cdf(d1) - K * np.exp(-r * T) * norm.cdf(d2)
-
-price = black_scholes_call(S=100, K=100, T=30/365, r=0.05, sigma=0.25)
-print(price)
-recovered_sigma = implied_volatility(price, S=100, K=100, T=30/365, r=0.05)
-print(recovered_sigma)
+    return multiplier * (S * norm.cdf(multiplier * d1) - K * np.exp(-r * T) * norm.cdf(multiplier * d2))
 
     
+
+avg_iv, call_iv, put_iv = get_atm_iv("SPY", 30)
+print(f"Avg IV: {avg_iv:.2%}")
+print(f"Call IV: {call_iv:.2%}")
+print(f"Put IV: {put_iv:.2%}")
